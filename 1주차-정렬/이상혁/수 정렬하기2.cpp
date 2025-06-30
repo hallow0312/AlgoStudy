7f202ff71321eab5dbf29483abb3315e01ee71ce
KaneBigNose <imddongs@naver.com>
2025-06-30 14:58:56 +0900

    Feat: 수 정렬하기2

---
+++ 1주차-정렬/이상혁/수 정렬하기2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int* arr;
+
+void input_nums();
+
+void print_nums();
+
+int main()
+{
+	input_nums();
+
+	sort(arr, arr + n);
+
+	print_nums();
+}
+
+void input_nums()
+{
+	cin >> n;
+
+	arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+void print_nums()
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\n";
+	}
+}
