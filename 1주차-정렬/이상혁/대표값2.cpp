8bf4046a90df0a81539df266a2a5ba4f08764e91
KaneBigNose <imddongs@naver.com>
2025-06-30 14:40:03 +0900

    Feat: 대표값2

---
+++ 1주차-정렬/이상혁/대표값2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void input_nums(int* arr);
+
+int avg_nums(int* arr);
+int middle_nums(int* arr);
+
+int main()
+{
+	int arr[5];
+
+	input_nums(arr);
+
+	sort(arr, arr + 5);
+
+	cout << avg_nums(arr) << "\n" << middle_nums(arr);
+}
+
+void input_nums(int* arr)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+int avg_nums(int* arr)
+{
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		sum += arr[i];
+	}
+
+	return sum / 5;
+}
+
+int middle_nums(int* arr)
+{
+	return arr[2];
+}
