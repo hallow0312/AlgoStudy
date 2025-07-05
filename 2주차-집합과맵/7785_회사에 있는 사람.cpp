38abc6d73ee81e11865d20abdf9938f37361f3ab
KaneBigNose <imddongs@naver.com>
2025-07-05 23:54:02 +0900

    Solve: 7785_회사에 있는 사람
    "현재 회사에 있는 사람의 이름을 사전 순의 역순으로 한 줄에 한 명씩 출력한다."는 문구도 못보고, 중복으로 enter하는 경우를 생각을 못했음

---
+++ 2주차-집합과맵/7785_회사에 있는 사람.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, string> input;
+vector<string> arr;
+
+void input_words();
+void find_words();
+
+int main()
+{
+	input_words();
+
+	sort(arr.begin(), arr.end(), greater<>());
+
+	arr.erase(unique(arr.begin(), arr.end()), arr.end());
+
+	find_words();
+}
+
+void input_words()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string name, log;
+		cin >> name >> log;
+		if (log == "enter")
+		{
+			arr.push_back(name);
+			input.insert(make_pair(name, name));
+		}
+		else if (log == "leave")
+		{
+			input.erase(name);
+		}
+	}
+}
+
+void find_words()
+{
+	for (int i = 0; i < arr.size(); i++)
+	{
+		if (input.count(arr[i]) == 1)
+		{
+			cout << arr[i] << "\n";
+		}
+	}
+}
