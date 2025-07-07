5512f5ded9d382e4d94b1cede1591592744a4e41
KaneBigNose <imddongs@naver.com>
2025-07-07 22:53:40 +0900

    Solve: 11478_서로 다른 부분 문자열의 개수
    string의 substr 함수가 있는 것을 첨 알았다. 이 함수 없었으면 내가 직접 자르면 시간복잡도 박살났을듯 함

---
+++ 2주차-집합과 맵/11478_서로 다른 부분 문자열의 개수.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+unordered_set<string> word_set;
+
+string word;
+int sum = 0;
+
+void input_word();
+void check_word();
+
+int main()
+{
+	input_word();
+	check_word();
+	cout << sum;
+}
+
+void input_word()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> word;
+}
+
+void check_word()
+{
+	for (int i = 0; i < word.size(); i++)
+	{
+		for (int j = 1; j <= word.size() - i; j++)
+		{
+			string temp = word.substr(i, j);
+			if (word_set.find(temp) == word_set.end())
+			{
+				word_set.insert(temp);
+				sum++;
+			}
+		}
+	}
+}
