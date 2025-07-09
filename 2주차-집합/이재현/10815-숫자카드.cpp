8c2c5c513afa43057b3ab845eff84bcf97c0c584
hallow0312 <leejaehyun0312@naver.com>
2025-07-10 02:45:03 +0900

    Init:2주차 파일  생성

---
+++ 2주차-집합/이재현/10815-숫자카드.cpp
@@ -0,0 +1,36 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define SIZE 500001
+int n, m;
+
+void  Init()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	vector<int>vec;
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		vec.push_back(a);
+	}
+	cin >> m;
+	sort(vec.begin(), vec.end());
+	for (int i = 0; i < m; i++)
+	{
+		int  a;
+		cin >> a;
+		if (binary_search(vec.begin(), vec.end(), a))
+		{
+			cout << 1 << ' ';
+		}
+		else cout << 0 << ' ';
+	}
+}
+int main()
+{
+	Init();
+	return 0;
+}
---
+++ 2주차-집합/이재현/14425-문자열집합.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int n, m;
+void  Init()
+{
+	int index = 0;
+
+	set<string>_set;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		string s; 
+		cin >> s;
+		_set.insert(s);
+	}
+	for (int i = 0; i < m; i++)
+	{
+		string s;
+		cin >> s;
+		if (_set.find(s) == _set.end())continue;
+		index++;
+	}
+	cout << index << '\n';
+}
+int main()
+{
+	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	Init();
+	return 0;
+}
