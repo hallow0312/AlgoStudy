b8813cb811e358fe8c9c8c0d8f1737853b8b9af8
KaneBigNose <imddongs@naver.com>
2025-07-12 22:21:15 +0900

    9012_괄호

---
+++ 3주차-스택큐덱/이상혁/9012_괄호.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+bool check_ghwalho(string temp)
+{
+	stack<char> stk;
+
+	for (char i : temp)
+	{
+		if (i == '(')
+		{
+			stk.push('(');
+		}
+		else if (i == ')')
+		{
+			if (stk.empty())
+			{
+				return false;
+			}
+			stk.pop();
+		}
+	}
+
+	if (!stk.empty())
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void input()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		string temp;
+		cin >> temp;
+		if (check_ghwalho(temp))
+		{
+			cout << "YES\n";
+		}
+		else
+		{
+			cout << "NO\n";
+		}
+	}
+}
+
+int main()
+{
+	input();
+}
