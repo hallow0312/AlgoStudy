a62c9bba35dd40a1cc6ecdf7a77e1d0087c3bb6e
KaneBigNose <imddongs@naver.com>
2025-07-17 20:46:43 +0900

    2164_카드2

---
+++ 3주차-스택큐덱/이상혁/2164_카드2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<int> que;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		que.push(i + 1);
+	}
+
+	int last;
+	bool is_push = false;
+
+	while (!que.empty())
+	{
+		last = que.front();
+
+		if (is_push)
+		{
+			que.push(que.front());
+			que.pop();
+		}
+		else
+		{
+			que.pop();
+		}
+
+		is_push = !is_push;
+	}
+
+	cout << last << "\n";
+}
