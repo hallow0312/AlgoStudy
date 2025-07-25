a433afcd343b9ca398376a5b24c8d6678153f2f7
KaneBigNose <imddongs@naver.com>
2025-07-25 16:28:17 +0900

    4주차 푼것들

---
+++ 4주차-재귀/이상혁/25501_재귀의 귀재.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int recursion(const string& s, int l, int r, int*& count)
+{
+	if (l >= r)
+	{
+		return 1;
+	}
+	else if (s[l] != s[r])
+	{
+		return 0;
+	}
+
+	(*count)++;
+
+	return recursion(s, l + 1, r - 1, count);
+}
+
+int isPalindrome(const string& s, int*& count)
+{
+	return recursion(s, 0, s.size() - 1, count);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	string temp;
+	int* count = new int;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		*count = 1;
+		int result = isPalindrome(temp, count);
+		cout << result << " " << *count << "\n";
+	}
+}
---
+++ 4주차-재귀/이상혁/27433_팩토리얼2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long fac = 1;
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		fac *= i + 1;
+	}
+
+	cout << fac << "\n";
+}
