336c4a5674a8c8e0739643b50222bed0a01f132f
KaneBigNose <imddongs@naver.com>
2025-07-28 23:46:53 +0900

    10989_수 정렬하기3

---
+++ 1주차-정렬/이상혁/10989_수 정렬하기3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+int arr[10001] = {};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		arr[temp]++;
+	}
+
+	for (int i = 0; i <= 10000; i++)
+	{
+		if (arr[i] > 0)
+		{
+			for (int j = 0; j < arr[i]; j++)
+			{
+				cout << i << "\n";
+			}
+		}
+	}
+}
