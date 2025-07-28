4a88fb1c5963afdfe656c18e3976d6ac6c8104fb
hallow0312 <leejaehyun0312@naver.com>
2025-07-29 04:07:45 +0900

    Feat: 4주차 재귀

---
+++ 4주차-재귀/이재현/10870-피보나치수 5.cpp
@@ -0,0 +1,35 @@
+﻿#include"bits/stdc++.h"
+using namespace std;
+#define IOS_BASE ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define SIZE 21
+int num;
+int Dynamic[SIZE] = { 0, };
+
+int Fibonacci(int n)
+{
+	if (n <= 0)
+	{
+		return Dynamic[n] = 0;
+	}
+	if (n <= 1)
+	{
+		return Dynamic[n] = 1;
+	}
+
+	if (Dynamic[n] != 0)
+		return Dynamic[n];
+	Dynamic[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+	return Dynamic[n];
+}
+
+void Init()
+{
+	cin >> num;
+	cout << Fibonacci(num);
+}
+
+int  main()
+{
+	Init();
+	return 0;
+}
---
+++ 4주차-재귀/이재현/11729-하노이탑.cpp
@@ -0,0 +1,36 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define IOS_BASE ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int num;
+
+void Hanoi(int num, int start, int middle, int end)
+{
+    if (num == 1)
+    {
+        cout << start << " " << end << '\n';
+        return;
+    }
+    Hanoi(num - 1,start, end ,middle );
+    cout << start << " " << end << '\n';
+    Hanoi(num - 1, middle, start, end);
+        
+}
+void Init()
+{
+    cin >> num;
+    int result = 1;
+    for (int i = 0; i < num; i++)
+    {
+        result *= 2;
+    }
+    cout << result - 1 << '\n';
+    Hanoi(num, 1, 2, 3);
+}
+
+int main()
+{
+    IOS_BASE
+        Init();
+
+    return 0;
+}
