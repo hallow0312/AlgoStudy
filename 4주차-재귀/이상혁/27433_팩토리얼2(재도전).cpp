58ffccb6a16dedf978af034704313075100b876b
KaneBigNose <imddongs@naver.com>
2025-07-28 16:15:11 +0900

    27433_팩토리얼2(재도전)

---
+++ 4주차-재귀/이상혁/27433_팩토리얼2(재도전).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+long long fac = 1;
+
+void facto()
+{
+	for (int i = 0; i < n; i++)
+	{
+		fac *= i + 1;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	facto();
+
+	cout << fac << "\n";
+}
