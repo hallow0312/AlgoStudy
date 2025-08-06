3a05c516145cac926cab2e756ccc7299e71cd369
KaneBigNose <imddongs@naver.com>
2025-08-06 16:27:51 +0900

    24416_알고리즘 수업 - 피보나치 수1

---
+++ 6주차-동적계획법-1/이상혁/24416_알고리즘 수업 - 피보나치 수1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+int count1 = 0, count2 = 0;
+
+int arr[41] = { 0,1,1,0 };
+
+int fibo1(int target)
+{
+	if (target == 1 || target == 2)
+	{
+		count1++;
+
+		return 1;
+	}
+
+	return fibo1(target - 2) + fibo1(target - 1);
+}
+
+int fibo2(int target)
+{
+	for (int i = 3; i <= target; i++)
+	{
+		arr[i] = arr[i - 2] + arr[i - 1];
+		count2++;
+	}
+
+	return arr[target];
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	fibo1(n);
+	fibo2(n);
+
+	cout << count1 << " " << count2 << "\n";
+}
