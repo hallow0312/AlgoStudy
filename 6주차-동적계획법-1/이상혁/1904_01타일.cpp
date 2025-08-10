54f318fcb871cc37203b2aed0146e27b6640b20d
KaneBigNose <imddongs@naver.com>
2025-08-10 18:57:03 +0900

    1904_01타일

---
+++ 6주차-동적계획법-1/이상혁/1904_01타일.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+int arr[1000000] = { 1,2, };
+
+int find_num(int target)
+{
+	for (int i = 2; i < target; i++)
+	{
+		arr[i] = (arr[i - 2] + arr[i - 1]) % 15746;
+	}
+
+	return arr[target - 1];
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	cout << find_num(n) << "\n";
+}
