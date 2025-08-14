4b5f238f9dd5ddafeb3da7f010f4f4c9aa0863f4
KaneBigNose <imddongs@naver.com>
2025-08-14 16:24:11 +0900

    1912_연속합

---
+++ 6주차-동적계획법-1/이상혁/1912_연속합.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+int arr[100001] = {};
+
+int find(int target)
+{
+	int value = INT32_MIN;
+
+	for (int i = 1; i <= n; i++)
+	{
+		arr[i] = max(arr[i - 1] + arr[i], arr[i]);
+		value = max(arr[i], value);
+	}
+
+	return value;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << find(n) << "\n";
+}
