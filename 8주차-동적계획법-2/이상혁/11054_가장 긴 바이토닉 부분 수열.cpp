8ffc8414265ff3c27711b537746340c8a2424c37
KaneBigNose <imddongs@naver.com>
2025-08-26 23:59:22 +0900

    11054_가장 긴 바이토닉 부분 수열

---
+++ 8주차-동적계획법-2/이상혁/11054_가장 긴 바이토닉 부분 수열.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+int arr[1001] = {};
+int sum[1001][2] = {};
+
+int find_front(int target)
+{
+	if (target <= 0)
+	{
+		return 0;
+	}
+
+	if (sum[target][0] != 0)
+	{
+		return sum[target][0];
+	}
+
+	int temp = 1;
+
+	for (int i = target - 1; i >= 1; i--)
+	{
+		if (arr[target] > arr[i])
+		{
+			temp = max(temp, find_front(i) + 1);
+		}
+	}
+
+	sum[target][0] = temp;
+
+	return temp;
+}
+
+int find_back(int target)
+{
+	if (target > n)
+	{
+		return 0;
+	}
+
+	if (sum[target][1] != 0)
+	{
+		return sum[target][1];
+	}
+
+	int temp = 1;
+
+	for (int i = target + 1; i <= n; i++)
+	{
+		if (arr[target] > arr[i])
+		{
+			temp = max(temp, find_back(i) + 1);
+		}
+	}
+
+	sum[target][1] = temp;
+
+	return temp;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int temp = 0;
+
+	for (int i = 1; i <= n; i++)
+	{
+		temp = max(temp, find_front(i) + find_back(i) - 1);
+	}
+
+	cout << temp << "\n";
+}
