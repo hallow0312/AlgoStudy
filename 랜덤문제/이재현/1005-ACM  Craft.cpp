207042f3652de05e395d995dea120b761828b547
hallow0312 <leejaehyun0312@naver.com>
2025-11-09 21:44:22 +0900

    달팽이

--- 랜덤문제/이재현/1005-ACM  Craft.cpp
+++ 랜덤문제/이재현/1005-ACM  Craft.cpp
@@ -1,79 +1,47 @@
 ﻿#include <iostream>
-#include <vector>
-#include <queue>
 #include <cstring>
+#define LL long long 
 using namespace std;
-int n, k, w, t, x, y;
-#define SIZE 1001
 
-int Hour[SIZE];
-int Degree[SIZE];
-int Dynamic[SIZE];
-vector<int> vec[SIZE];
+LL m, n;
 
-
-void  Init()
+void Init()
 {
-    cin >> t;
+    cin >> m >> n;
 
-    while (t--)
+    LL layer = (min(m, n) - 1) / 2;
+    LL cnt = layer * 4;
+    LL re_m = m - layer * 2;
+    LL re_n = n - layer * 2;
+    LL x = 1 + layer;
+    LL y = 1 + layer;
+
+    if (re_m == 1 && re_n == 1) {}
+    else if (re_m == 1)  y += (re_n - 1);
+    else if (re_n == 1)
     {
-
-        cin >> n >> k;
-
-        for (int i = 0; i <= n; i++)
-        {
-            vec[i].clear();
-            Degree[i] = 0;
-            Dynamic[i] = 0;
-        }
-
-        for (int i = 1; i <= n; i++)
-            cin >> Hour[i];
-
-        for (int i = 0; i < k; i++)
-        {
-
-            cin >> x >> y;
-            vec[x].push_back(y);
-            Degree[y]++;
-        }
-
-        cin >> w;
-        queue<int> q;
-
-        for (int i = 1; i <= n; i++)
-        {
-            if (Degree[i] == 0)
-            {
-                q.push(i);
-                Dynamic[i] = Hour[i];
-            }
-        }
-
-        while (!q.empty())
-        {
-            int cur = q.front();
-            q.pop();
-
-            for (int nxt : vec[cur]) {
-                Degree[nxt]--;
-                Dynamic[nxt] = max(Dynamic[nxt], Dynamic[cur] + Hour[nxt]);
-
-                if (Degree[nxt] == 0)
-                    q.push(nxt);
-            }
-        }
-
-        cout << Dynamic[w] << '\n';
+        cnt += 1;
+        x += (re_m - 1);
     }
+    else if (re_m == 2) {
+
+        cnt += 2;
+        x++;
+    }
+    else if (re_n == 2) {
+
+        cnt += 3;
+        x++;
+    }
+
+    cout << cnt << '\n';
+    cout << x << ' ' << y << '\n';
 }
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     Init();
-
     return 0;
 }
